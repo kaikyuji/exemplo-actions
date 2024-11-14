53ddeefd8e946377305520a197cebf246af1e2c4
kaikyuji <kaikykohatsu@gmail.com>
2024-11-13 21:30:36 -0300

    Should throw exception

--- src/test/java/sptech/school/exemplo_actions/services/CalculadoraServiceTest.java
+++ src/test/java/sptech/school/exemplo_actions/services/CalculadoraServiceTest.java
@@ -1,8 +1,11 @@
 package sptech.school.exemplo_actions.services;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
 import sptech.school.exemplo_actions.service.CalculadoraService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CalculadoraServiceTest {
     @Test
     @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
@@ -13,5 +16,20 @@ public class CalculadoraServiceTest {
         double esperado = 5;
         double resultado = service.dividir(a, b);
         assertEquals(esperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 0, então deve lançar uma exceção")
+    public void testDividirPorZero() {
+        CalculadoraService service = new CalculadoraService();
+        double a = 10;
+        double b = 0;
+        var expectedMessage = "400 BAD_REQUEST \"Divisão por zero não permitida\"";
+
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> { 
+            service.dividir(a, b);
+        });
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
